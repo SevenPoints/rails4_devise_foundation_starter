f7a4e8df331a438ebeb9b0c9669fc8984d78916b
James McGrath <jim@sevenpoints.co>
2014-10-05 08:31:28 -0400

    production.rb changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'sevenpoints-timecard.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
