8a6fbc57cfcc7949fd6c49d7db5da64ab05eb624
James McGrath <jim@sevenpoints.co>
2014-10-05 09:57:38 -0400

    sendgrid changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,26 +78,26 @@ Rails.application.configure do
    config.action_mailer.raise_delivery_errors = true
    config.action_mailer.default :charset => "utf-8"
   
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: ENV["GMAIL_DOMAIN"],
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
-  }
+  #config.action_mailer.smtp_settings = {
+  #  address: "smtp.gmail.com",
+  #  port: 587,
+  #  domain: ENV["GMAIL_DOMAIN"],
+  #  authentication: "plain",
+  #  enable_starttls_auto: true,
+  #  user_name: ENV["GMAIL_USERNAME"],
+  #  password: ENV["GMAIL_PASSWORD"]
+  #}
   #Config for SendGrid
   #config/environments/production.rb
-  #  config.action_mailer.smtp_settings = {
-  #    :address        => 'smtp.sendgrid.net',
-  #    :port           => '587',
-  #    :authentication => :plain,
-  #    :user_name      => ENV['SENDGRID_USERNAME'],
-  #    :password       => ENV['SENDGRID_PASSWORD'],
-  #    :domain         => 'heroku.com',
-  #    :enable_starttls_auto => true
-  #  }
+    config.action_mailer.smtp_settings = {
+      :address        => 'smtp.sendgrid.net',
+      :port           => '587',
+      :authentication => :plain,
+      :user_name      => ENV['SENDGRID_USERNAME'],
+      :password       => ENV['SENDGRID_PASSWORD'],
+      :domain         => 'heroku.com',
+      :enable_starttls_auto => true
+    }
   
  
   # Disable automatic flushing of the log to improve performance.
